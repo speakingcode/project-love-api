8021fda31069f954ef7c9e3fbfd9ef4de1495922
speakingcode <dlissner@gmail.com>
2017-01-27 15:59:28 -0600

    Set up jwt authentication

---
+++ app/controllers/api/v1/home_controller.rb
@@ -0,0 +1,9 @@
+module Api::V1
+  class HomeController < ApplicationController
+    before_filter :authenticate_request!
+  
+    def index
+      render json: {'logged_in' => true}
+    end
+  end
+end
